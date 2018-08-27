e0a43b28a851997d68f9aa460819bdd45a52598b
abhijeet-choudhary <42260750+abhijeet-choudhary@users.noreply.github.com>
2018-08-27 21:19:47 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,18 @@
+package experiment;
+
+public class Test {
+	public static void main(String bby[]){
+		int i=0;
+		for(int j=1;j<10;j++){
+			
+			if(i==5){
+				i=0;
+			}
+				i++;
+				System.out.println(j+"  "+i);
+				
+			
+		}
+		
+	}
+}
